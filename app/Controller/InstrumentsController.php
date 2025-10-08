a5c684b6d1d45fd6f2d19d5b531a5bd9be46eb35
21Killua <120782177+21Killua@users.noreply.github.com>
2025-10-08 19:16:23 +0100

    add2 page and controller changes

--- app/Controller/InstrumentsController.php
+++ app/Controller/InstrumentsController.php
@@ -10,30 +10,30 @@ App::uses('OutilsController', 'Controller');
  */
 class InstrumentsController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
+	/**
+	 * Components
+	 *
+	 * @var array
+	 */
 	public $components = array('Paginator');
 
-/**
- * index method
- *
- * @return void
- */
+	/**
+	 * index method
+	 *
+	 * @return void
+	 */
 	public function index() {
 		$this->Instrument->recursive = 0;
 		$this->set('instruments', $this->Paginator->paginate());
 	}
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
+	/**
+	 * view method
+	 *
+	 * @throws NotFoundException
+	 * @param string $id
+	 * @return void
+	 */
 	public function view($id = null) {
 		if (!$this->Instrument->exists($id)) {
 			throw new NotFoundException(__('Invalid instrument'));
@@ -42,11 +42,11 @@ class InstrumentsController extends AppController {
 		$this->set('instrument', $this->Instrument->find('first', $options));
 	}
 
-/**
- * add method
- *
- * @return void
- */
+	/**
+	 * add method
+	 *
+	 * @return void
+	 */
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Instrument->create();
@@ -64,13 +64,13 @@ class InstrumentsController extends AppController {
 		$this->set(compact('compositions', 'fournisseurs'));
 	}
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
+	/**
+	 * edit method
+	 *
+	 * @throws NotFoundException
+	 * @param string $id
+	 * @return void
+	 */
 	public function edit($id = null) {
 		if (!$this->Instrument->exists($id)) {
 			throw new NotFoundException(__('Invalid instrument'));
@@ -91,13 +91,101 @@ class InstrumentsController extends AppController {
 		$this->set(compact('compositions', 'fournisseurs'));
 	}
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
+	public function add2() {
+		$this->layout = false;
+
+		if ($this->request->is('post')) {
+			// Handle image upload
+			if (!empty($this->request->data['Instrument']['image_file']['name'])) {
+				$file = $this->request->data['Instrument']['image_file'];
+				$fileName = time() . '_' . $file['name'];
+				$uploadPath = WWW_ROOT . 'files' . DS . $fileName;
+
+				if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
+					// Store the relative path in the database
+					$this->request->data['Instrument']['image'] = 'files/' . $fileName;
+				} else {
+					$this->Flash->error(__('Error uploading image.'));
+				}
+
+				// Remove the file input from data to avoid saving it
+				unset($this->request->data['Instrument']['image_file']);
+			}
+
+			$this->Instrument->create();
+			if ($this->Instrument->save($this->request->data)) {
+				$this->Session->setFlash(__('The instrument has been saved.'));
+
+				// Return JSON response for AJAX
+				if ($this->request->is('ajax')) {
+					$this->autoRender = false;
+					echo json_encode(array('success' => true, 'message' => 'Instrument saved successfully'));
+					return;
+				}
+
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				// Return JSON error for AJAX
+				if ($this->request->is('ajax')) {
+					$this->autoRender = false;
+					$this->response->statusCode(400);
+					echo json_encode(array('success' => false, 'message' => 'Could not save instrument'));
+					return;
+				}
+
+				$this->Flash->error(__('The instrument could not be saved. Please, try again.'));
+			}
+		}
+
+		$outils = new OutilsController();
+		$this->request->data['Instrument']['ref'] = $outils->generatecodebar('set');
+		$compositions = $this->Instrument->Composition->find('list');
+		$fournisseurs = $this->Instrument->Fournisseur->find('list');
+		$this->set(compact('compositions', 'fournisseurs'));
+	}
+
+
+	public function getFournisseurs() {
+		// Important: Configure the view to return JSON
+		$this->layout = 'ajax';
+		$this->autoRender = false;
+
+		// Set proper headers for JSON
+		$this->response->type('json');
+		header('Content-Type: application/json');
+
+		try {
+			$fournisseurs = $this->Instrument->Fournisseur->find('all', array(
+				'fields' => array('Fournisseur.id', 'Fournisseur.name'),
+				'order' => array('Fournisseur.name' => 'ASC'),
+				'recursive' => -1
+			));
+
+			// Format data for autocomplete
+			$result = array();
+			foreach ($fournisseurs as $fournisseur) {
+				$result[] = array(
+					'id' => $fournisseur['Fournisseur']['id'],
+					'name' => $fournisseur['Fournisseur']['name']
+				);
+			}
+
+			echo json_encode($result);
+		} catch (Exception $e) {
+			echo json_encode(array('error' => $e->getMessage()));
+		}
+
+		return;
+	}
+
+
+	/**
+	 * delete method
+	 *
+	 * @throws NotFoundException
+	 * @param string $id
+	 * @return void
+	 */
 	public function delete($id = null) {
 		if (!$this->Instrument->exists($id)) {
 			throw new NotFoundException(__('Invalid instrument'));
---
+++ app/View/Instruments/add2.ctp
@@ -0,0 +1,98 @@
+<div class="instruments form">
+	<?php echo $this->Form->create('Instrument'); ?>
+	<div class="page-header">
+		<h1 class="title-page">Instrument</h1>
+		<span class="slogan"></span>
+	</div>
+	<div class="row">
+		<div class="col"></div>
+		<div class="col-8">
+
+			<div class="row">
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('composition_id', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('fournisseur_id', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('ref', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('name', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('nom_comercial', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('code_fournisseur', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('num_lot', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('prix', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('etat', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('date_ajout', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('taille', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('remarque', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('image', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('coordonnees_image', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='col-12'>
+					<?php
+					echo $this->Form->input('type_lavage', array('placeholder'=>''));
+					?>
+				</div>
+				<div class='submit-section'>
+					<button type="submit" class="btn btn-submit">
+						<i class="fa-solid fa-paper-plane"></i> Envoyer
+					</button>
+				</div><?php echo $this->Form->end(); ?>
+			</div>
+		</div>
+		<div class="col"></div>
+
+
+	</div>
+</div>
